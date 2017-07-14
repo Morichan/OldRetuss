ddcbb4b408f8936eb1eca76fd76845a1b4ec76c1
Morichan <nintendo698@gmail.com>
2017-07-15 00:48:52 +0900

    パッケージ名を変更

--- src/retuss/Controller.java
+++ src/retuss/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package retuss;
 
 public class Controller {
 }
--- src/retuss/Main.java
+++ src/retuss/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package retuss;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
